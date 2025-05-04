a6d96509b8db341ee1b159649e9a86198600940e
mattphillips-one <mattphillips142@gmail.com>
2025-05-03 23:53:40 -0400

    Configured for server

--- src/app/components/CardList.tsx
+++ src/app/components/CardList.tsx
@@ -2,7 +2,7 @@ import Card from '@/app/components/ui/Card';
 import { NewsFeed, NewsItem } from '@/app/types/types';
 
 export default async function CardList({ query }: { query: string }) {
-  const response: Response = await fetch(`${process.env.URL}/api/search?query=${query}`);
+  const response: Response = await fetch(`${process.env.SERVER_URL}/api/search?query=${query}`);
   const { result } = await response?.json();
 
   return (
