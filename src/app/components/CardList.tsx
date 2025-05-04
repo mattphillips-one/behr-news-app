cd391198613ee40d382718bd4338855adf358e44
mattphillips-one <mattphillips142@gmail.com>
2025-05-04 00:02:02 -0400

    Set server port

--- src/app/components/CardList.tsx
+++ src/app/components/CardList.tsx
@@ -2,7 +2,8 @@ import Card from '@/app/components/ui/Card';
 import { NewsFeed, NewsItem } from '@/app/types/types';
 
 export default async function CardList({ query }: { query: string }) {
-  const response: Response = await fetch(`${process.env.SERVER_URL}/api/search?query=${query}`);
+  const port = process.env.URL || 'https://behr-news-app.vercel.app';
+  const response: Response = await fetch(`${port}/api/search?query=${query}`);
   const { result } = await response?.json();
 
   return (
