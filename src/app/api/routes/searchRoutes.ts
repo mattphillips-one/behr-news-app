d8ac9ea369fbe1a398d224ae12848154d5d4f8e4
mattphillips-one <mattphillips142@gmail.com>
2025-05-03 23:56:31 -0400

    Removed unnecessary dependencies

--- src/app/api/routes/searchRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { SearchController } from '../controllers/searchController.js';
-
-const router = Router();
-
-router.get('/', SearchController.search);
-
-export default router;
