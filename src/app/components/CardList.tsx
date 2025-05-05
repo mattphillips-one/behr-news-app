13c67779dd950956fae757a08c24d3e6b998d38c
mattphillips-one <mattphillips142@gmail.com>
2025-05-05 15:07:41 -0400

    Removed api folder

--- src/app/components/CardList.tsx
+++ src/app/components/CardList.tsx
@@ -8,13 +8,13 @@ import { NewsFeed, NewsItem } from '@/app/types/types';
 */
 
 export default async function CardList({ query }: { query: string }) {
-  const port = process.env.URL || 'https://behr-news-app.vercel.app';
-  const response = await fetch(`${port}/api/search?query=${query}`);
+  const port = 'https://behr-api.vercel.app';
+  const response = await fetch(`${port}/search?query=${query}`);
   const { results } = await response.json();
 
   return (
-    <section className='max-w-2xl mx-6 lg:pl-[10%]'>
-      <ul className='flex flex-col gap-6'>
+    <section className='max-w-3xl self-center'>
+      <ul className='flex flex-col gap-6 md:mr-[10%]'>
         {results?.map((newsItem: NewsItem, i: number) => {
           return (
             <li key={i}>
