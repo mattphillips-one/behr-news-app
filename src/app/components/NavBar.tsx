ff5c73ca7b6269d48a28f1ece6fea2f6f802238d
mattphillips-one <mattphillips142@gmail.com>
2025-05-17 21:23:21 -0400

    fixed navbar black dark mode

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { fonts } from "./ui/fonts";
 
 export default function Navbar() {
   return (
-    <nav className='flex flex-row items-center md:items-baseline w-full text-white border-b-1 border-neutral-300 dark:border-neutral-500 px-5 pb-2 justify-between bg-neutral-900 dark:bg-neutral-900'>
+    <nav className='flex flex-row items-center md:items-baseline w-full text-white border-b-1 border-neutral-300 dark:border-neutral-500 px-5 pb-2 justify-between bg-neutral-900 dark:bg-black'>
       <a href='/' className={`${fonts.stix} font-bold text-white text-xl md:text-2xl w-3/4 md:w-full text-left pt-3`}>
         Business Environment & Human Rights News
       </a>
